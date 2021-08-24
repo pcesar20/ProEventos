67567459ced3e00cda48697396faa1c292ba7e87
Paulo Cesar <paulocesar.araujo.ads@gmail.com>
2021-08-24 11:18:47 -0300

    teste

---
+++ back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+
+        public IEnumerable<Evento> _evento = new Evento[]{
+        new Evento() {
+               EventoId = 1,
+               Tema = "teste",
+               Local = "Goiania",
+               Lote = "1 Lote",
+               QtdPessoas = 250,
+               DataEvento = DateTime.Now.AddDays(2).ToString(),
+               ImagemURL = "teste.png"
+                      },
+                       new Evento() {
+               EventoId = 2,
+               Tema = "teste 2",
+               Local = "Aparecida",
+               Lote = "1 Lote",
+               QtdPessoas = 250,
+               DataEvento = DateTime.Now.AddDays(2).ToString(),
+               ImagemURL = "teste.png"
+                       }
+    };
+     
+        public EventoController()
+        {
+            
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+           return _evento;
+                
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<Evento> Get(int id)
+        {
+           return _evento.Where(evento => evento.EventoId == id);
+                
+        }
+
+
+    
+    }
+}
